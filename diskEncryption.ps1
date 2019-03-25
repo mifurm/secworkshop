869d903504d3a9c25f52508b615654df17b928ff
mifurm <michal.furmankiewicz@hotmail.com>
2019-03-25 13:40:09 +0100

    Update diskEncryption.ps1

--- diskEncryption.ps1
+++ diskEncryption.ps1
@@ -1,1 +1,16 @@
 #diskEncryption
+
+#encrypt the disk
+$rgName = 'securityWorkshopRG';
+$vmName = 'vm01diskenc';
+$KeyVaultName = 'keyva01';
+$KeyVault = Get-AzureRmKeyVault -VaultName $KeyVaultName -ResourceGroupName $rgname;
+$diskEncryptionKeyVaultUrl = $KeyVault.VaultUri;
+$KeyVaultResourceId = $KeyVault.ResourceId;
+
+Set-AzureRmVMDiskEncryptionExtension -ResourceGroupName $rgName -VMName $vmName -DiskEncryptionKeyVaultUrl $diskEncryptionKeyVaultUrl -DiskEncryptionKeyVaultId $KeyVaultResourceId -Force;
+
+#validate disk encryption
+$rgName = 'securityWorkshopRG';
+$vmName = 'vm01diskenc';
+Get-AzureRmVmDiskEncryptionStatus -ResourceGroupName $rgName -VMName $vmName
