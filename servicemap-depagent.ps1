d915a7bffa6d60eebf36a444d4bdc5aa4cfced67
mifurm <michal.furmankiewicz@hotmail.com>
2020-11-19 23:51:36 +0100

    Update servicemap-depagent.ps1

--- servicemap-depagent.ps1
+++ servicemap-depagent.ps1
@@ -1,10 +1,13 @@
+Set-AzContext -Subscription "mifurmlab"
 $version = "9.4"
 $ExtPublisher = "Microsoft.Azure.Monitoring.DependencyAgent"
 $OsExtensionMap = @{ "Windows" = "DependencyAgentWindows"; "Linux" = "DependencyAgentLinux" }
 #choose propoer sec group
-$rmgroup = "sec01rg"
-
-Get-AzureRmVM -ResourceGroupName $rmgroup |
+#$rmgroup = "sec01rg"
+$rgs=Get-AzureRmResourceGroup
+ForEach ($rg in $rgs)
+{
+Get-AzureRmVM -ResourceGroupName $rg.ResourceGroupName |
 ForEach-Object {
     ""
     $name = $_.Name
@@ -16,7 +19,8 @@ ForEach-Object {
     Date -Format o
     $ext = $OsExtensionMap.($os.ToString())
     $result = Set-AzureRmVMExtension -ResourceGroupName $vmRmGroup -VMName $name -Location $location `
-    -Publisher $ExtPublisher -ExtensionType $ext -Name "DependencyAgent" -TypeHandlerVersion $version
+    -Publisher $ExtPublisher -ExtensionType $ext -Name "DependencyAgent" -TypeHandlerVersion $version -NoWait
     $result.IsSuccessStatusCode
     }
+}
 }
