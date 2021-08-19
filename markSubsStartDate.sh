0106ddd92abe391e886b125be8b7354edd2e9865
mifurm <michal.furmankiewicz@hotmail.com>
2021-08-19 23:10:37 +0200

    Update markSubsStartDate.sh

--- markSubsStartDate.sh
+++ markSubsStartDate.sh
@@ -1,1 +1,9 @@
 az tag create --resource-id "/subscriptions/ffca029c-a6e3-4630-9dfc-ff43256cd2f8" --tags StartDate=20.08.2021
+
+az account list --query [].id > subs.list
+for n in $(cat subs.list) 
+do 
+        temp=$(eval echo $n)
+        temp=${temp:0:36}
+        az tag list --resource-id "/subscriptions/$temp"
+done
